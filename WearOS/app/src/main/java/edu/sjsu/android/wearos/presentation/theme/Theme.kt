8c6d5376a3b19f968085481af06f7f8b3586db52
Jasmine Lao <jasmine.lao@sjsu.edu>
2024-09-03 15:19:31 -0700

    0 setup project1

---
+++ WearOS/app/src/main/java/edu/sjsu/android/wearos/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package edu.sjsu.android.wearos.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WearOSTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
