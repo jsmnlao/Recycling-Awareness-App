ea0d196f5084b2303133a8dc1d7775226214a292
Jasmine Lao <jasmine.lao@sjsu.edu>
2024-09-06 11:04:10 -0700

    fixed display issue

--- Exercise1JasmineLao/app/src/main/java/edu/sjsu/android/exercise1jasminelao/MainActivity.java
+++ Exercise1JasmineLao/app/src/main/java/edu/sjsu/android/exercise1jasminelao/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Set onClickListener to the button and pass onClick method
         binding.button.setOnClickListener(this::onClick);
-        onClick(view);
     }
 
     /**
@@ -58,12 +57,12 @@ public class MainActivity extends AppCompatActivity {
             binding.textView.setText(String.valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
             // Switch to the other option
             binding.radioButton.setChecked(false);
-            binding.radioButton2.setChecked(false);
+            binding.radioButton2.setChecked(true);
         }
         else{
             binding.textView.setText(String.valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
-            binding.radioButton.setChecked(false);
-            binding.radioButton2.setChecked(true);
+            binding.radioButton.setChecked(true);
+            binding.radioButton2.setChecked(false);
         }
     }
 }
