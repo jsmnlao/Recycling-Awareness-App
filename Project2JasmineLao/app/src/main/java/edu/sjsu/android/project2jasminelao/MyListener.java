8e0e70d0c88cb5cb4e2768647562c57e0e9b9d9d
Jasmine Lao <jasmine.lao@sjsu.edu>
2024-09-19 15:48:34 -0700

    updated MyListener

--- Project2JasmineLao/app/src/main/java/edu/sjsu/android/project2jasminelao/MyListener.java
+++ Project2JasmineLao/app/src/main/java/edu/sjsu/android/project2jasminelao/MyListener.java
@@ -4,34 +4,62 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.display.DisplayManager;
 import android.view.Display;
+import android.view.Surface;
 
 public class MyListener implements SensorEventListener {
 
-    private Display display;
+    private final Display display;
     private float x, y;
     private long timestamp;
 
     public MyListener(Context context){
         // TODO: initialize display object using context object passed in
+        display = ((DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE)).getDisplay(Display.DEFAULT_DISPLAY);
         x = 0f;
         y = 0f;
         timestamp = 0;
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        // TODO: implement on 9/19
-        /*
-        Set x-axis, y-axis, and timestamp value based on the sensor data. Remember to interpret the
-        data based on different orientations (0, 90, 180, 270) of the screen. Lesson 8 page 17 gives the
-        pseudocode for rotation 0 and 90 as examples.
-         */
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        timestamp = sensorEvent.timestamp;
+
+        int rotation = display.getRotation();
+        if(rotation == Surface.ROTATION_0){
+            x = sensorEvent.values[0];
+            y = sensorEvent.values[1];
+        }
+        else if(rotation == Surface.ROTATION_90){
+            x = -sensorEvent.values[1];
+            y = sensorEvent.values[0];
+        }
+        else if(rotation == Surface.ROTATION_180){
+            x = -sensorEvent.values[0];
+            y = -sensorEvent.values[1];
+        }
+        else if(rotation == Surface.ROTATION_270){
+            x = sensorEvent.values[1];
+            y = -sensorEvent.values[0];
+        }
     }
 
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public long getTimestamp(){
+        return timestamp;
     }
 }
